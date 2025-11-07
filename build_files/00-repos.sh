19639cc570e50850bb2f03477e4a40efafd1b6aa
JianZcar <esteban.jianzcar@outlook.com>
2025-11-07 17:07:58 +0800

    fix

--- build_files/00-repos.sh
+++ build_files/00-repos.sh
@@ -36,5 +36,5 @@ done
 
 dnf5 -y config-manager setopt "*".exclude="*.aarch64"
 dnf5 -y config-manager setopt "*fedora*".priority=1 "*fedora*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
-dnf5 -y config-manager setopt "*akmods*".priority=2
-eval "$(/ctx/helper/dnf5-setopt.sh setopt '*negativo17*' priority=3 exclude='mesa-* *xone*')"
+# dnf5 -y config-manager setopt "*akmods*".priority=2
+# eval "$(/ctx/helper/dnf5-setopt.sh setopt '*negativo17*' priority=3 exclude='mesa-* *xone*')"
