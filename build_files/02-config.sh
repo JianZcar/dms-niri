e08922024fb608a66248bf70de1c4befc5198230
JianZcar <esteban.jianzcar@outlook.com>
2025-11-11 11:39:03 +0800

    fix

--- build_files/02-config.sh
+++ build_files/02-config.sh
@@ -6,10 +6,11 @@ set -ouex pipefail
 
 /ctx/helper/config-apply.sh
 
-echo "debugging"
-cat /etc/pam.d/greetd
-getent passwd greeter
-groups greeter
+mkdir -p /etc/systemd/system/dms.service.d
+cat <<'EOF' > /etc/systemd/system/dms.service.d/override.conf
+[Unit]
+ConditionUser=!greeter
+EOF
 
 sed -i 's/^#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 sed -i 's/^#LockLayering=false/LockLayering=true/' /etc/rpm-ostreed.conf
